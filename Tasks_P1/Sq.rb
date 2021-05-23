beb66a6e60211a8dc040dd104af15b71b9079b54
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2021-05-23 20:03:41 +0300

    new changes

--- Tasks_P1/Sq.rb
+++ Tasks_P1/Sq.rb
@@ -1,21 +1,21 @@
-puts "What is coefficient a?"
-a=gets.chomp
+puts 'What is coefficient a?'
+a = gets.chomp
 
-puts "What is coefficient b?"
-b=gets.chomp
+puts 'What is coefficient b?'
+b = gets.chomp
 
-puts "What is coefficient c?"
-c=gets.chomp
+puts 'What is coefficient c?'
+c = gets.chomp
 
-d=b.to_i**2-4*a.to_i*c.to_i
+d = b.to_i**2 - 4 * a.to_i * c.to_i
 
-if d>0
-    x1=((-1)*b.to_i + Math.sqrt(d.to_i))/(a.to_i*2)
-    x2=((-1)*b.to_i - Math.sqrt(d.to_i))/(a.to_i*2)
-    puts "Results: Discriminant=#{d}, X1=#{x1.round(2)}, X2=#{x2.round(2)}"
-elsif d==0
-    x=((-1)*b.to_i)/(a.to_i*2)
-    puts "Results: Discriminant=#{d.round(2)}, X=#{x.round(2)}"
+if d > 0
+  x1 = (-1 * b.to_i + Math.sqrt(d.to_i)) / (a.to_i * 2)
+  x2 = (-1 * b.to_i - Math.sqrt(d.to_i)) / (a.to_i * 2)
+  puts "Results: Discriminant=#{d}, X1=#{x1.round(2)}, X2=#{x2.round(2)}"
+elsif d == 0
+  x = (-1 * b.to_i) / (a.to_i * 2)
+  puts "Results: Discriminant=#{d.round(2)}, X=#{x.round(2)}"
 else
-    puts "Results: No roots, Discriminant=#{d.round(2)}"
-end    
+  puts "Results: No roots, Discriminant=#{d.round(2)}"
+end
