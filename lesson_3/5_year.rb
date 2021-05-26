99396981df096d3d05cff7bcf1ad954a196c0247
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2021-05-26 21:34:20 +0300

    tasks  5, 6

---
+++ lesson_3/5_year.rb
@@ -0,0 +1,36 @@
+months = { 1 => 31,
+           2 => 28,
+           3 => 31,
+           4 => 30,
+           5 => 31,
+           6 => 30,
+           7 => 31,
+           8 => 31,
+           9 => 30,
+           10 => 31,
+           11 => 30,
+           12 => 31 }
+
+puts 'day?'
+day_answ = gets.chomp.to_i
+
+puts 'month?'
+month_answ = gets.chomp.to_i
+
+puts 'year?'
+year_answ = gets.chomp.to_i
+
+if year_answ % 400 == 0 || (year_answ % 4 == 0) && (year_answ % 100 != 0)
+  months[2] = 29
+end
+
+full_sum = 0
+months.each_key do |month_num|
+  if month_num <= month_answ
+    full_sum +=  months[month_num]
+  end
+end
+
+full_sum += day_answ
+
+puts full_sum
---
+++ lesson_3/6_shopping.rb
@@ -0,0 +1,29 @@
+shopping_cart = {}
+shopping_cart_sum = {}
+sc_sum = 0
+
+loop do
+  puts 'What is material name?'
+  puts 'To finish please type -stop-'
+  material = gets.chomp
+
+  break if material == 'stop'
+
+  puts 'What is material quantity?'
+  quantity = gets.chomp.to_f
+
+  puts 'What is material price?'
+  price = gets.chomp.to_f
+
+  shopping_cart[material] = [quantity, price]
+  if shopping_cart_sum[material].nil?
+    shopping_cart_sum[material] = quantity * price
+  else
+    shopping_cart_sum[material] += quantity * price
+  end
+  sc_sum += quantity * price
+end
+
+puts shopping_cart
+puts shopping_cart_sum
+puts "Final sum: #{sc_sum}"
