799898dfe18478b6b7130d51a68d3c376b886397
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2021-05-26 13:10:35 +0300

    Update 4_letters.rb
    added extra hash and () in If

--- lesson_3/4_letters.rb
+++ lesson_3/4_letters.rb
@@ -1,14 +1,13 @@
-abc = ('a'..'z').to_a
+abc = ('a'..'z')
 vowels = ['a', 'e', 'i', 'o', 'u']
 
-abc_hash = Hash.new
+vowels_hash = Hash.new
 k=1
 abc.each { |letter| 
-    abc_hash[letter] = k
-    if vowels.include? letter.to_s == false
-     abc_hash.delete[letter]
+    if (vowels.include? letter ) == true
+        vowels_hash[letter]=k
     end
     k += 1
 }
 
-puts abc_hash
+puts vowels_hash
