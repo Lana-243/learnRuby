662780004c77fd223c2f8eba8ec4940918dfbed3
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2021-05-26 22:02:21 +0300

    added symbols

--- lesson_3/1_months.rb
+++ lesson_3/1_months.rb
@@ -1,12 +1,21 @@
-months = { 'january' => 31,
-           'February' => 28,
-           'March' => 31,
-           'April' => 30,
-           'May' => 31,
-           'June' => 30,
-           'July' => 31,
-           'August' => 31,
-           'September' => 30,
-           'October' => 31,
-           'November' => 30,
-           'December' => 31 }
+months = { january: 31,
+           February: 28,
+           March: 31,
+           April: 30,
+           May: 31,
+           June: 30,
+           July: 31,
+           August: 31,
+           September: 30,
+           October: 31,
+           November: 30,
+           December: 31 }
+
+months_30 = []
+months.each do |month_name, days|
+  if days == 30
+      months_30 << month_name
+  end
+end
+
+puts months_30
