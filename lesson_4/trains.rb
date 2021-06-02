62a7c61c3b46b2680e873071a845f7235e396b26
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2021-06-02 22:56:58 +0300

    Update trains.rb

--- lesson_4/trains.rb
+++ lesson_4/trains.rb
@@ -1,101 +1,115 @@
 class Station
   def initialize(station_name)
     @station_name = station_name
-    @passenger_trains_at_station = []
-    @cargo_trains_at_station = []
+    @passenger_trains = []
+    @cargo_trains = []
   end
 
-  def add_train_to_station(train_number, train_type)
-    case train_type
+  def add_train_to_station(name)
+    case name.type
     when 'Cargo'
-      @cargo_trains_at_station << train_number
+      @cargo_trains << name
     when 'Passenger'
-      @passenger_trains_at_station << train_number
-    else
-      puts 'Please enter correct train type: Cargo or Passenger'
+      @passenger_trains << name
     end
+  end
+
+  def all_trains
+    @all_trains = @passenger_trains + @cargo_trains
   end
 
   def trains_list_full
-    puts "Trains at the station #{@passenger_trains_at_station + @cargo_trains_at_station}"
+    @all_trains.each { |name| puts name}
   end
 
   def trains_list_by_type
-    puts "Passanger trains: #{@passenger_trains_at_station}; Cargo trains: #{@cargo_trains_at_station}"
+    puts "Passanger trains: #{@passenger_trains.map(&:name).join(‘, ’)}; Cargo trains: #{@cargo_trains.map(&:name).join(‘, ’)}"
   end
 
   def train_leaving(train_leaving_name)
-    @train_leaving_name = train_leaving_name
-    if (@passenger_trains_at_station + @cargo_trains_at_station).include? train_leaving_name
-      @passenger_trains_at_station.delete_if { |passenger| passenger.include? train_leaving_name }
-      @cargo_trains_at_station.delete_if { |cargo| cargo.include? train_leaving_name }
-      puts "Train #{train_leaving_name} has left the station"
-    else
-      puts 'There is no such train at the station'
-    end
-  end
-end
+    if (@passenger_trains + @cargo_trains).include? train_leaving_name
+      @passenger_trains.delete_if { |passenger| passenger.include? train_leaving_name }
+      @cargo_trains.delete_if { |cargo| cargo.include? train_leaving_name }
+    end  
+ end
 
 class Route
-  def initialize(first_station, last_station)
-    @route = [first_station, last_station]
+  attr_reader :route
+
+  def initialize(route, first_station, last_station)
+    @route_name = route
+    @route_stations = [first_station, last_station]
   end
 
-  def add_station_to_route(middle_station)
-    @route.insert(-2, middle_station)
+  def add_station(middle_station)
+    @route_stations.insert(-2, middle_station)
   end
 
-  def del_station_from_route(station_to_del)
-    if @route.include? station_to_del
-      @route.delete(station_to_del)
-      puts "Station #{station_to_del} has been deleted"
-    else
-      puts 'Station you want to delete is not on the route'
-    end
+  def trains_list_full
+    @route_stations.each { |station| puts station}
   end
 
-  def route_show
-    puts "The route: #{@route}"
+  def del_station(station_to_del)
+      @route_stations.delete(station_to_del) if @route_stations.include? station_to_del
   end
-end
 
 class Train
-  def initialize(train_number, train_type, carriages_number)
-    @train_number = train_number
+  attr_reader :type, :speed, :carriages_number
+  def initialize(name, type, carriages_number)
+    @name = name
     @train_type_list = {}
-    @train_type_list[train_number] = train_type
+    @train_type_list[name] = type
     @carriages_number = carriages_number
     @speed = 0
   end
 
   def train_speed_add(speed)
-    if speed >= 0
-      @speed += speed
-    else
-      puts 'Please enter speed that is higher than 0!'
-    end
-  end
-
-  def train_speed_show
-    puts "Current speed: #{@speed}"
+    @speed += speed if speed > 0
   end
 
   def train_stop
     @speed = 0
-    puts 'Train has stopped'
-  end
-
-  def carriages_number_show
-    puts "Number of carriages for #{@train_number} is #{@carriages_number}"
   end
 
   def carriages_number_change(change_car)
-    if @speed == 0 && (carriages_number + change_car) > 0
+    if @speed.zero? && (carriages_number + change_car) > 0
       @carriages_number += change_car
-    elsif (carriages_number + change_car) <= 0
-      puts "Number of carriages is #{@carriages_number} and we cannot change it by #{change_car}"
-    else
-      puts 'Please stop the train first!'
     end
   end
+
+  def route_add(route)
+    @route = route
+    @station_index = 0
+    @route.route_stations[@station_index].@add_train_to_station(self)
+  end  
+
+  def route_add(route)
+    @route = route
+    @station_index = 0
+    @route.route_stations[@station_index].@add_train_to_station(self)
+  end  
+
+  def move_fwd
+    @route.curr_station.train_leaving(self)
+    @station_index += 1
+    @route.curr_station.add_train_to_station(self)
+  end
+
+  def move_bckwd
+    @route.curr_station.train_leaving(self)
+    @station_index -= 1
+    @route.curr_station.add_train_to_station(self)
+  end
+
+  def curr_station
+    @route.route_stations[@station_index]
+  end
+
+  def next_station
+    @route.route_stations[@station_index+1]
+  end
+
+   def prev_station
+    @route.route_stations[@station_index-1] if station_index > 0
+   end
 end
