e6d5c5fa4e2563a3edcbf05f437ab6e7fd49a9e3
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2021-06-04 21:23:07 +0300

    Update trains.rb

--- lesson_4/trains.rb
+++ lesson_4/trains.rb
@@ -1,61 +1,54 @@
 class Station
+  attr_reader :trains, :station_name
+
   def initialize(station_name)
     @station_name = station_name
-    @passenger_trains = []
-    @cargo_trains = []
+    @trains = []
   end
 
-  def add_train_to_station(train)
-    case train.type
-    when 'Cargo'
-      @cargo_trains << train
-    when 'Passenger'
-      @passenger_trains << train
-    end
+  def add_train(train)
+    @trains << train
   end
 
-  def all_trains
-    @passenger_trains + @cargo_trains
-  end
-
-  def trains_list_full
-    all_trains.map(&:train).join(‘, ’)
+  def trains_list
+    @trains.map(&:name).join(', ')
   end
 
   def trains_list_by_type
-    puts "Passenger trains: #{@passenger_trains.map(&:train).join(‘, ’)}; Cargo trains: #{@cargo_trains.map(&:train).join(‘, ’)}"
+    passenger_trains = @trains.select { |train| train.type == 'Passenger' }.map(&:name).join(', ')
+    cargo_trains = @trains.select { |train| train.type == 'Cargo' }.map(&:name).join(', ')
+    puts "Passenger trains: #{passenger_trains}; Cargo trains: #{cargo_trains}"
   end
 
-  def train_leaving(train_leaving)
-    if (@passenger_trains + @cargo_trains).include? train_leaving
-      @passenger_trains.delete_if { |passenger| passenger.include? train_leaving }
-      @cargo_trains.delete_if { |cargo| cargo.include? train_leaving }
-    end  
- end
+  def train_leaving(train)
+    @trains.delete(train) if @trains.include?(train)
+  end
+end
 
 class Route
-  attr_reader :route
+  attr_reader :route, :first_station
 
   def initialize(route, first_station, last_station)
-    @route_name = route
+    @route = route
     @route_stations = [first_station, last_station]
   end
 
   def add_station(middle_station)
-    @route_stations.insert(-2, middle_station)
+    @route.insert(-2, middle_station)
   end
 
   def trains_list_full
-    @route_stations.map(&:train).join(‘, ’)
+    @stations.map(&:station_name).join(‘, ’)
   end
 
-  def del_station(station_to_del)
-      @route_stations.delete(station_to_del) if @route_stations.include? station_to_del
+  def delete_station(station)
+    @stations.delete(station) if @stations.include? station
   end
-end 
+end
 
 class Train
   attr_reader :name, :type, :speed, :carriages_number
+
   def initialize(name, type, carriages_number)
     @name = name
     @type = type
@@ -63,7 +56,7 @@ class Train
     @speed = 0
   end
 
-  def train_speed_add(speed)
+  def add_speed(speed)
     @speed += speed if speed > 0
   end
 
@@ -72,44 +65,40 @@ class Train
   end
 
   def carriages_number_change(change_car)
-    if @speed.zero? && (carriages_number + change_car) > 0
-      @carriages_number += change_car
-    end
-  end 
+    @carriages_number += change_car if @speed.zero? && (carriages_number + change_car).positive?
+  end
 
-  def route_add(route)
+  def add_route(route)
     @route = route
     @station_index = 0
-    @route.route_stations[@station_index].@add_train_to_station(self)
-  end  
+    @route.stations[0].add_train(self)
+  end
 
-  def route_add(route)
-    @route = route
-    @station_index = 0
-    @route.route_stations[@station_index].@add_train_to_station(self)
-  end  
+  def current_station
+    @route.stations[@station_index]
+  end
 
-  def move_fwd
-    @route.curr_station.train_leaving(self)
+  def move_forward
+    @route.current_station.train_leaving(self)
     @station_index += 1
-    @route.curr_station.add_train_to_station(self)
+    @route.current_station.add_train(self)
   end
 
-  def move_bckwd
-    @route.curr_station.train_leaving(self)
+  def move_backward
+    @route.current_station.train_leaving(self)
     @station_index -= 1
-    @route.curr_station.add_train_to_station(self)
+    @route.current_station.add_train(self)
   end
 
-  def curr_station
-    @route.route_stations[@station_index]
+  def current_station
+    @route.stations[@station_index]
   end
 
   def next_station
-    @route.route_stations[@station_index+1]
+    @route.stations[@station_index + 1]
   end
 
-   def prev_station
-    @route.route_stations[@station_index-1] if station_index > 0
-   end
+  def prev_station
+    @route.stations[@station_index - 1] if station_index > 0
+  end
 end
